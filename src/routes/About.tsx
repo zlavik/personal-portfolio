bd78606371274eb5ba26dc8cc0cac03f660a9979
Slavik F <kivalz74@gmail.com>
2024-03-29 00:33:34 -0700

    joke about me

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -9,16 +9,16 @@ const About = () => {
         <div className="dashboard-card main">
           <div className="dashboard-card-content">
             <img src="https://cdn.wetransfer.net/assets/transfer_window/transfer_error-5cb9b360b9f6c8dbcc744c9cb436e3ff3d66aeda055c6a588f71b6ee61588e27.jpg" alt="img"/>
-            <h2>Dan Covic</h2>
-            <p>CTO @ Le Wagon</p>
+            <h2>Slavik Ferris</h2>
+            <p>Wololo</p>
             <button className="btn">Get in touch</button>
           </div>
         </div>
  
         <div className="dashboard-card description aboutMe">
           <div className="dashboard-card-content">
-            <h3>More about Dan</h3>
-            <p className="text-justify">I am full-stack dev with 5 years of experience in startups.</p>               </div>
+            <h3>More about Slavik</h3>
+            <p className="text-justify">Pls job</p>               </div>
         </div>
       </div>
     </div>

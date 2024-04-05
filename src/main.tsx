a8192782b768a7c3425d845fc855e558f7961bd3
Slavik F <kivalz74@gmail.com>
2024-04-05 13:22:32 -0700

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import {
     RouterProvider,
     createBrowserRouter
 } from 'react-router-dom'
-import App from './App'
 import Home from './routes/Home';
 import FinanceApp from './routes/FinanceApp';
 import React from 'react';
+import NavBar from './routes/NavBar';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />,
+        element: <NavBar />,
         children: [
             {
                 path: '/',
@@ -26,8 +26,9 @@ const router = createBrowserRouter([
     }
 ]);
  
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!)
+    .render(
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>,
+    )
